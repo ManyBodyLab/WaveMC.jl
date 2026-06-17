82d859c87b092b8697d0636be606de68ce7a9be9
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2026-06-17 18:41:53 +0200

    fix first acceptance rate adaptation

--- src/acceptance_rate/acceptance_rate.jl
+++ src/acceptance_rate/acceptance_rate.jl
@@ -7,7 +7,7 @@ function adapt_acceptance_rate!(mc::AbstractWavefunctionMC, ::MCContext, ::NoAcc
 end
 function adapt_acceptance_rate!(mc::MC, ctx::MCContext, adapter::AcceptanceAdapter; verbosity::Int = 1) where {MC <: AbstractWavefunctionMC}
     mc_state = state(mc)
-    if !is_thermalized(ctx) && mod1(ctx.sweeps, adapter.adapt_interval) == adapter.adapt_interval
+    if !is_thermalized(ctx) && ctx.sweeps >= adapter.adapt_interval && mod(ctx.sweeps, adapter.adapt_interval) == 0
         acc_rate = acceptance_rate(mc)
         if verbosity > 0
             str = @sprintf("Adapt acceptance rate: Current acceptance rate = %.6f, target = %.6f", acc_rate, adapter.acceptance_rate)
